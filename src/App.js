affee25dbb9e57a479db2a332d9e9b80c958c9eb
zMyrddin <janmarvin.consul@gmail.com>
2023-09-28 23:16:57 +1000

    feat: Added HomePage and Browser Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Myrddin's Pokedex App!</h1>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Seen/AddSeen" />
+        <Route path="/Seen/AddSeen" />
+        <Route path="/Caught" />
+
+
+      </Routes>
+
     </div>
   );
 }
